2937b8abf1d188d79a20866d1d5ecdf7854a078f
claypigeon123 <Zoltan.Nagy@student.shu.ac.uk>
2020-02-14 15:54:48 +0000

    Update LockedContent.js

--- src/components/LockedContent.js
+++ src/components/LockedContent.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 export class LockedContent extends Component {
     render() {
-        if (this.props.activeUser._id === null) {
-            return null;
-        } else {
+        if (this.props.activeUser._id !== null) {
             return (
                 <React.Fragment>
                     <h5 className="text-danger font-weight-bold">This is content only visible when logged in.</h5>
